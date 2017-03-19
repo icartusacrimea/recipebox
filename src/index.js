3af4909bd64bc1b87785929c6c6147d07428047f
icartusacrimea <icartusacrimea@gmail.com>
2017-03-19 00:39:55 -0400

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import RecipeBox from './App';
-import './index.css';
-
-ReactDOM.render(
-  <RecipeBox />,
-  document.querySelector('.container')
-);
